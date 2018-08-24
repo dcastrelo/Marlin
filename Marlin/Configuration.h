49f29debcbf31b89c2e298dd19611e02758c9c95
Dario <castrelo@gmail.com>
2018-08-23 23:08:52 -0300

    steps per unit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -597,11 +597,14 @@
  * Default Axis Steps Per Unit (steps/mm)
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
+ * L3K con engranajes rectos                                     
  */
-L3K con engranajes rectos
+
 // #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 595 }
 
-Drakon Extruder
+/**
+ * Drakon Extruder                                      
+ */
 #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 348.56 }
 
 
