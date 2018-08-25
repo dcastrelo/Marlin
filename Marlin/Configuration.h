0850cab642d023667a31b5ae4f7a341bac196979
Dario <castrelo@gmail.com>
2018-08-25 00:02:52 -0300

    volvimos al l3k

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -599,13 +599,12 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  * L3K con engranajes rectos                                     
  */
-
-// #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 595 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 607.94 }
 
 /**
  * Drakon Extruder                                      
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 348.56 }
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 348.56 }
 
 
 /**
@@ -776,7 +775,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 32  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -3.9   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -4   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
